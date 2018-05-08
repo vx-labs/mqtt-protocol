d3c611617ece003345b23712d7571ef70808c62e
Julien BONACHERA <jbonachera@xebia.fr>
2018-05-08 17:51:55 +0200

    pb: add suback

---
+++ pb/suback.go
@@ -0,0 +1,14 @@
+package pb
+
+import "encoding/binary"
+
+func EncodeSubAck(p *MqttSubAck, buff []byte) (int, error) {
+	total := 0
+	binary.BigEndian.PutUint16(buff, uint16(p.MessageId))
+	total += 2
+	for _, qos := range p.Qos {
+		buff[total] = byte(qos)
+		total++
+	}
+	return total, nil
+}
---
+++ pb/suback_test.go
@@ -0,0 +1,31 @@
+package pb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuback_Encode(t *testing.T) {
+	buff := make([]byte, 10)
+	packet := &MqttSubAck{
+		Header:    &MqttHeader{},
+		MessageId: 9,
+		Qos:       []int32{1, 0, 2, 1},
+	}
+	n, err := EncodeSubAck(packet, buff)
+	assert.Nil(t, err)
+	assert.Equal(t, 6, n)
+}
+func BenchmarkSuback_Encode(b *testing.B) {
+	buff := make([]byte, 10)
+	packet := &MqttSubAck{
+		Header:    &MqttHeader{},
+		MessageId: 9,
+		Qos:       []int32{1, 0, 2, 1},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		EncodeSubAck(packet, buff)
+	}
+}
