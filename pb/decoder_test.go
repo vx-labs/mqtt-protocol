307a3291594574dfa6374213127b38e48fafc597
Julien BONACHERA <jbonachera@xebia.fr>
2018-05-08 14:12:24 +0200

    pb: bench short and long packet decoding

--- pb/decoder_test.go
+++ pb/decoder_test.go
@@ -34,12 +34,27 @@ func TestDecoder_ReadMessageBuffer_Long(t *testing.T) {
 }
 func BenchmarkDecoder_ReadMessageBuffer(b *testing.B) {
 	buff := []byte{0x32, 0x7, 0x0, 0x1, 'a', 0x0, 0x1, 'p', 'a'}
+	longBuff := []byte{0x32, 0x81, 0x1,
+		0x0, 0x1, 'a',
+		0x0, 0x83}
+	for i := 0; i < 0x83; i++ {
+		buff = append(longBuff, 'a')
+	}
 	reader := bytes.NewReader(buff)
 	d := NewDecoder()
 	p := &MqttHeader{}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		d.readMessageBuffer(p, reader)
-		reader.Reset(buff)
-	}
+	b.Run("short", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			reader.Reset(buff)
+			d.readMessageBuffer(p, reader)
+		}
+	})
+	b.Run("long", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			reader.Reset(longBuff)
+			d.readMessageBuffer(p, reader)
+		}
+	})
 }
