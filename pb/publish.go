8c5efb688d5d1422eed08924cc8b3d44cacbdcbb
Julien BONACHERA <jbonachera@xebia.fr>
2018-05-08 16:10:37 +0200

    pb: add encodePublish method

--- pb/publish.go
+++ pb/publish.go
@@ -1,6 +1,9 @@
 package pb
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"errors"
+)
 
 func decodePublish(p *MqttPublish, buff []byte) (int, error) {
 	topic, n, err := decodeLP(buff)
@@ -16,6 +19,27 @@ func decodePublish(p *MqttPublish, buff []byte) (int, error) {
 	}
 	p.Payload = buff[total:]
 	return len(buff), nil
+}
+func encodePublish(p *MqttPublish, buff []byte) (int, error) {
+	total := 0
+	n, err := encodeLP(p.Topic, buff)
+	total += n
+	if err != nil {
+		return total, err
+	}
+	if p.Header != nil && p.Header.Qos > 0 {
+		if len(buff[total:]) < 2 {
+			return total, errors.New("buffer to short to encode message id")
+		}
+		binary.BigEndian.PutUint16(buff[total:], uint16(p.MessageId))
+		total += 2
+	}
+	if len(buff[total:]) < len(p.Payload) {
+		return total, errors.New("buffer to short to encode message payload")
+	}
+	copy(buff[total:], p.Payload)
+	total += len(p.Payload)
+	return total, nil
 }
 
 type publishHandler func(*MqttPublish) error
--- pb/publish_test.go
+++ pb/publish_test.go
@@ -51,3 +51,41 @@ func BenchmarkPublish_Decode_QoS1(b *testing.B) {
 		decodePublish(p, buff)
 	}
 }
+func TestPublish_Encode_QoS0(t *testing.T) {
+	buff := make([]byte, 6)
+	p := &MqttPublish{
+		Header:  &MqttHeader{Qos: 0},
+		Topic:   []byte("a"),
+		Payload: []byte{1, 2, 3},
+	}
+	n, err := encodePublish(p, buff)
+	assert.Nil(t, err)
+	assert.Equal(t, 6, n)
+	assert.Equal(t, []byte{0x0, 0x1, 'a', 0x1, 0x2, 0x3}, buff)
+}
+func TestPublish_Encode_QoS1(t *testing.T) {
+	buff := make([]byte, 8)
+	p := &MqttPublish{
+		Header:    &MqttHeader{Qos: 1},
+		Topic:     []byte("a"),
+		Payload:   []byte{1, 2, 3},
+		MessageId: 9,
+	}
+	n, err := encodePublish(p, buff)
+	assert.Nil(t, err)
+	assert.Equal(t, 8, n)
+	assert.Equal(t, []byte{0x0, 0x1, 'a', 0x0, 0x9, 0x1, 0x2, 0x3}, buff)
+}
+func BenchmarkPublish_Encode_QoS0(b *testing.B) {
+	buff := make([]byte, 8)
+	p := &MqttPublish{
+		Header:    &MqttHeader{Qos: 1},
+		Topic:     []byte("a"),
+		Payload:   []byte{1, 2, 3},
+		MessageId: 9,
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encodePublish(p, buff)
+	}
+}
