2ea304216343d92bf5c31ba0e7eb7eacb3c70d99
Julien BONACHERA <jbonachera@xebia.fr>
2018-05-09 10:42:55 +0200

    pb: add connack

---
+++ pb/connack.go
@@ -0,0 +1,23 @@
+package pb
+
+import (
+	"errors"
+)
+
+const (
+	CONNACK_CONNECTION_ACCEPTED int32 = iota
+	CONNACK_REFUSED_BAD_PROTOCOL_VERSION
+	CONNACK_REFUSED_IDENTIFIER_REJECTED
+	CONNACK_REFUSED_SERVER_UNAVAILABLE
+	CONNACK_REFUSED_BAD_USERNAME_OR_PASSWORD
+	CONNACK_REFUSED_NOT_AUTHORIZED
+)
+
+func EncodeConnAck(p *MqttConnAck, buff []byte) (int, error) {
+	if len(buff) < 2 {
+		return 0, errors.New("buffer to short to encode connack")
+	}
+	buff[0] = 0x0
+	buff[1] = byte(p.ReturnCode)
+	return 2, nil
+}
---
+++ pb/connack_test.go
@@ -0,0 +1,23 @@
+package pb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnAck_Encode(t *testing.T) {
+	b := make([]byte, 2)
+	p := &MqttConnAck{ReturnCode: CONNACK_CONNECTION_ACCEPTED}
+	n, err := EncodeConnAck(p, b)
+	assert.Nil(t, err)
+	assert.Equal(t, len(b), n)
+}
+
+func BenchmarkConnAck_Encode(b *testing.B) {
+	buff := make([]byte, 2)
+	p := &MqttConnAck{ReturnCode: CONNACK_CONNECTION_ACCEPTED}
+	for i := 0; i < b.N; i++ {
+		EncodeConnAck(p, buff)
+	}
+}
