f0ab98ad3ac12df22849c0753ef3dc3204a22757
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-04-07 12:19:47 +0200

    add more benchmarks

--- decoder/async_test.go
+++ decoder/async_test.go
@@ -24,16 +24,54 @@ func TestAsyncDecoder_Packets(t *testing.T) {
 	require.IsType(t, &packet.Publish{}, p)
 }
 func BenchmarkAsyncDecoder_decodeEncodedPacket(b *testing.B) {
-	buff := []byte{0x32, 55,
+	buf := []byte{}
+	buf = append(buf, 0x32, 55,
 		0x0, 0x1, 'a',
-		0, 50}
+		0, 50)
 	for i := 0; i < 50; i++ {
-		buff = append(buff, 'a')
+		buf = append(buf, 'a')
 	}
 	h := make([]byte, 4)
-	reader := bytes.NewReader(buff)
+	reader := bytes.NewReader(buf)
 	for i := 0; i < b.N; i++ {
 		reader.Seek(0, io.SeekStart)
 		decodeEncodedPacket(h, reader)
 	}
+}
+func BenchmarkAsyncDecoder_unmarshalPacket(b *testing.B) {
+	b.Run("pingreq", func(b *testing.B) {
+		header := &packet.Header{}
+		p := &packet.PingReq{Header: &packet.Header{}}
+		buff := make([]byte, p.Length())
+		_, err := p.Encode(buff)
+		require.NoError(b, err)
+		for i := 0; i < b.N; i++ {
+			unmarshalPacket(packet.PINGREQ, header, buff)
+		}
+	})
+	b.Run("publish", func(b *testing.B) {
+		header := &packet.Header{}
+		p := &packet.Publish{Header: &packet.Header{}, MessageId: 1, Payload: []byte("test"), Topic: []byte("test")}
+		buff := make([]byte, p.Length())
+		_, err := p.Encode(buff)
+		require.NoError(b, err)
+		for i := 0; i < b.N; i++ {
+			unmarshalPacket(packet.PUBLISH, header, buff)
+		}
+	})
+	b.Run("subscribe", func(b *testing.B) {
+		header := &packet.Header{}
+		p := &packet.Subscribe{
+			Header:    &packet.Header{},
+			MessageId: 1,
+			Qos:       []int32{0},
+			Topic:     [][]byte{[]byte("test")},
+		}
+		buff := make([]byte, p.Length())
+		_, err := p.Encode(buff)
+		require.NoError(b, err)
+		for i := 0; i < b.N; i++ {
+			unmarshalPacket(packet.SUBSCRIBE, header, buff)
+		}
+	})
 }
